1b24b81eb81f83544c0a9dd2bffd285e4365fe56
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-05-27 20:06:16 +0430

    fix check auth

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,7 +4,8 @@ const { verifyToken } = require("../utils/auth.util")
 async function checkAuth(req, res, next) {
     try {
         const authorization = req?.headers?.authorization
-        const { bearer, token } = authorization?.split(" ")
+        const [ bearer, token ] = authorization?.split(" ")
+
         if (bearer && bearer.toLowerCase() === "bearer") {
             if (token) {
                 const verifyResult = verifyToken(token)
